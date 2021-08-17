16f22d86f6ba5bdae145460e7881aa351984e25b
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-08-17 10:30:53 +0200

    Add files via upload

---
+++ SharpText/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace SharpText
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.line1 = new System.Windows.Forms.Label();
+            this.line2 = new System.Windows.Forms.Label();
+            this.line3 = new System.Windows.Forms.Label();
+            this.line6 = new System.Windows.Forms.Label();
+            this.line5 = new System.Windows.Forms.Label();
+            this.line4 = new System.Windows.Forms.Label();
+            this.line12 = new System.Windows.Forms.Label();
+            this.line11 = new System.Windows.Forms.Label();
+            this.line10 = new System.Windows.Forms.Label();
+            this.line9 = new System.Windows.Forms.Label();
+            this.line8 = new System.Windows.Forms.Label();
+            this.line7 = new System.Windows.Forms.Label();
+            this.line14 = new System.Windows.Forms.Label();
+            this.line13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // line1
+            // 
+            this.line1.AutoSize = true;
+            this.line1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line1.Location = new System.Drawing.Point(12, 9);
+            this.line1.Name = "line1";
+            this.line1.Size = new System.Drawing.Size(0, 29);
+            this.line1.TabIndex = 0;
+            // 
+            // line2
+            // 
+            this.line2.AutoSize = true;
+            this.line2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line2.Location = new System.Drawing.Point(12, 38);
+            this.line2.Name = "line2";
+            this.line2.Size = new System.Drawing.Size(0, 29);
+            this.line2.TabIndex = 1;
+            // 
+            // line3
+            // 
+            this.line3.AutoSize = true;
+            this.line3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line3.Location = new System.Drawing.Point(12, 67);
+            this.line3.Name = "line3";
+            this.line3.Size = new System.Drawing.Size(0, 29);
+            this.line3.TabIndex = 2;
+            // 
+            // line6
+            // 
+            this.line6.AutoSize = true;
+            this.line6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line6.Location = new System.Drawing.Point(12, 154);
+            this.line6.Name = "line6";
+            this.line6.Size = new System.Drawing.Size(0, 29);
+            this.line6.TabIndex = 5;
+            // 
+            // line5
+            // 
+            this.line5.AutoSize = true;
+            this.line5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line5.Location = new System.Drawing.Point(12, 125);
+            this.line5.Name = "line5";
+            this.line5.Size = new System.Drawing.Size(0, 29);
+            this.line5.TabIndex = 4;
+            // 
+            // line4
+            // 
+            this.line4.AutoSize = true;
+            this.line4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line4.Location = new System.Drawing.Point(12, 96);
+            this.line4.Name = "line4";
+            this.line4.Size = new System.Drawing.Size(0, 29);
+            this.line4.TabIndex = 3;
+            // 
+            // line12
+            // 
+            this.line12.AutoSize = true;
+            this.line12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line12.Location = new System.Drawing.Point(12, 328);
+            this.line12.Name = "line12";
+            this.line12.Size = new System.Drawing.Size(0, 29);
+            this.line12.TabIndex = 11;
+            // 
+            // line11
+            // 
+            this.line11.AutoSize = true;
+            this.line11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line11.Location = new System.Drawing.Point(12, 299);
+            this.line11.Name = "line11";
+            this.line11.Size = new System.Drawing.Size(0, 29);
+            this.line11.TabIndex = 10;
+            // 
+            // line10
+            // 
+            this.line10.AutoSize = true;
+            this.line10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line10.Location = new System.Drawing.Point(12, 270);
+            this.line10.Name = "line10";
+            this.line10.Size = new System.Drawing.Size(0, 29);
+            this.line10.TabIndex = 9;
+            // 
+            // line9
+            // 
+            this.line9.AutoSize = true;
+            this.line9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line9.Location = new System.Drawing.Point(12, 241);
+            this.line9.Name = "line9";
+            this.line9.Size = new System.Drawing.Size(0, 29);
+            this.line9.TabIndex = 8;
+            // 
+            // line8
+            // 
+            this.line8.AutoSize = true;
+            this.line8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line8.Location = new System.Drawing.Point(12, 212);
+            this.line8.Name = "line8";
+            this.line8.Size = new System.Drawing.Size(0, 29);
+            this.line8.TabIndex = 7;
+            // 
+            // line7
+            // 
+            this.line7.AutoSize = true;
+            this.line7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line7.Location = new System.Drawing.Point(12, 183);
+            this.line7.Name = "line7";
+            this.line7.Size = new System.Drawing.Size(0, 29);
+            this.line7.TabIndex = 6;
+            // 
+            // line14
+            // 
+            this.line14.AutoSize = true;
+            this.line14.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line14.Location = new System.Drawing.Point(12, 386);
+            this.line14.Name = "line14";
+            this.line14.Size = new System.Drawing.Size(0, 29);
+            this.line14.TabIndex = 13;
+            // 
+            // line13
+            // 
+            this.line13.AutoSize = true;
+            this.line13.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line13.Location = new System.Drawing.Point(12, 357);
+            this.line13.Name = "line13";
+            this.line13.Size = new System.Drawing.Size(0, 29);
+            this.line13.TabIndex = 12;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1010, 425);
+            this.Controls.Add(this.line14);
+            this.Controls.Add(this.line13);
+            this.Controls.Add(this.line12);
+            this.Controls.Add(this.line11);
+            this.Controls.Add(this.line10);
+            this.Controls.Add(this.line9);
+            this.Controls.Add(this.line8);
+            this.Controls.Add(this.line7);
+            this.Controls.Add(this.line6);
+            this.Controls.Add(this.line5);
+            this.Controls.Add(this.line4);
+            this.Controls.Add(this.line3);
+            this.Controls.Add(this.line2);
+            this.Controls.Add(this.line1);
+            this.MaximumSize = new System.Drawing.Size(1028, 472);
+            this.MinimumSize = new System.Drawing.Size(1028, 472);
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Main_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label line1;
+        private System.Windows.Forms.Label line2;
+        private System.Windows.Forms.Label line3;
+        private System.Windows.Forms.Label line6;
+        private System.Windows.Forms.Label line5;
+        private System.Windows.Forms.Label line4;
+        private System.Windows.Forms.Label line12;
+        private System.Windows.Forms.Label line11;
+        private System.Windows.Forms.Label line10;
+        private System.Windows.Forms.Label line9;
+        private System.Windows.Forms.Label line8;
+        private System.Windows.Forms.Label line7;
+        private System.Windows.Forms.Label line14;
+        private System.Windows.Forms.Label line13;
+    }
+}
+
---
+++ SharpText/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace SharpText
+{
+    public partial class Main : Form
+    {
+        bool Command_Mode = false;
+
+        string command = "";
+        string prompt = "";
+        string file = "";
+
+        int Selected_Line = 0;
+        int Display_From = 0;
+        List<string> Loaded_Lines = new List<string>() {};
+        List<string> To_Display = new List<string>() { "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
+
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+        void Update_Display_List()
+        {
+            int temp = Display_From;
+            for(int i = 0; i < 14; i++)
+            {
+                if(temp < Loaded_Lines.Count) To_Display[i] = Loaded_Lines[temp];
+                else To_Display[i] = "";
+                temp++;
+            }
+        }
+
+        void Render_Text()
+        {
+            line1.Text = To_Display[0];
+            line2.Text = To_Display[1];
+            line3.Text = To_Display[2];
+            line4.Text = To_Display[3];
+            line5.Text = To_Display[4];
+            line6.Text = To_Display[5];
+            line7.Text = To_Display[6];
+            line8.Text = To_Display[7];
+            line9.Text = To_Display[8];
+            line10.Text = To_Display[9];
+            line11.Text = To_Display[10];
+            line12.Text = To_Display[11];
+            line13.Text = To_Display[12];
+            line14.Text = To_Display[13];
+        }
+
+        void Render_Text_CommandMode(string newPrompt = null)
+        {
+            if(newPrompt != null) prompt = newPrompt;
+            line1.Text = prompt;
+            line2.Text = command;
+            line3.Text = "";
+            line4.Text = "";
+            line5.Text = "";
+            line6.Text = "";
+            line7.Text = "";
+            line8.Text = "";
+            line9.Text = "";
+            line10.Text = "";
+            line11.Text = "";
+            line12.Text = "";
+            line13.Text = "";
+            line14.Text = "";
+
+            line1.ForeColor = Color.Lime;
+            line2.ForeColor = Color.Lime;
+        }
+
+        void Render_Selected()
+        {
+            Color Selected_Color = Color.Lime;
+            Color Default_Color = Color.White;
+            if (Selected_Line == 0) line1.ForeColor = Selected_Color;
+            else line1.ForeColor = Default_Color;
+            if (Selected_Line == 1) line2.ForeColor = Selected_Color;
+            else line2.ForeColor = Default_Color;
+            if (Selected_Line == 2) line3.ForeColor = Selected_Color;
+            else line3.ForeColor = Default_Color;
+            if (Selected_Line == 3) line4.ForeColor = Selected_Color;
+            else line4.ForeColor = Default_Color;
+            if (Selected_Line == 4) line5.ForeColor = Selected_Color;
+            else line5.ForeColor = Default_Color;
+            if (Selected_Line == 5) line6.ForeColor = Selected_Color;
+            else line6.ForeColor = Default_Color;
+            if (Selected_Line == 6) line7.ForeColor = Selected_Color;
+            else line7.ForeColor = Default_Color;
+            if (Selected_Line == 7) line8.ForeColor = Selected_Color;
+            else line8.ForeColor = Default_Color;
+            if (Selected_Line == 8) line9.ForeColor = Selected_Color;
+            else line9.ForeColor = Default_Color;
+            if (Selected_Line == 9) line10.ForeColor = Selected_Color;
+            else line10.ForeColor = Default_Color;
+            if (Selected_Line == 10) line11.ForeColor = Selected_Color;
+            else line11.ForeColor = Default_Color;
+            if (Selected_Line == 11) line12.ForeColor = Selected_Color;
+            else line12.ForeColor = Default_Color;
+            if (Selected_Line == 12) line13.ForeColor = Selected_Color;
+            else line13.ForeColor = Default_Color;
+            if (Selected_Line == 13) line14.ForeColor = Selected_Color;
+            else line14.ForeColor = Default_Color;
+        }
+
+        void Load_File(string file_name)
+        {
+            file = file_name;
+            if (!File.Exists(file_name)) { var new_file = File.Create(file_name); new_file.Close(); Load_File(file_name);  return; }
+            string[] lines = System.IO.File.ReadAllLines(@file_name);
+            Command_Mode = false;
+            foreach (string line in lines)
+            {
+                Loaded_Lines.Add(line);
+            }
+            Enter_EditMode();
+        }
+
+        void Enter_CommandMode()
+        {
+            Command_Mode = true;
+            Render_Text_CommandMode("Open or create file");
+        }
+
+        void Enter_EditMode()
+        {
+            Render_Selected();
+            Update_Display_List();
+            Render_Text();
+        }
+
+        void Save_File()
+        {
+            TextWriter tw = new StreamWriter(file);
+            foreach (String s in Loaded_Lines)
+                tw.WriteLine(s);
+            tw.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Enter_CommandMode();        
+        }
+
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Command_Mode) return;
+            if (e.KeyCode == Keys.Up)
+            {
+                if (Selected_Line > 0) Selected_Line--;
+                else
+                {
+                    if (Display_From - 1 < 0) return;
+                    Display_From--;
+                    Update_Display_List();
+                    Render_Text();
+                }
+                Render_Selected();
+                return;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                if (Selected_Line < 13) Selected_Line++;
+                else
+                {
+                    if (Display_From + 14 > Loaded_Lines.Count) return;
+                    Display_From++;
+                    Update_Display_List();
+                    Render_Text();
+                }
+                if (Selected_Line + Display_From >= Loaded_Lines.Count) Loaded_Lines.Add("");
+                Render_Selected();
+                return;
+            }
+        }
+
+        private void Main_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Command_Mode)
+            {
+                if ((int)e.KeyChar == 13) { Load_File(command); return; }
+                if ((int)e.KeyChar == 8)
+                {
+                    if(command.Length > 0) command = command.Remove(command.Length - 1, 1);
+                    Render_Text_CommandMode();
+                    return;
+                }
+                command += e.KeyChar;
+                Render_Text_CommandMode();
+                return;
+            }
+
+            if ((int)e.KeyChar == 8)
+            {
+                if (Loaded_Lines[Selected_Line + Display_From].Length > 0)
+                {
+                    Loaded_Lines[Selected_Line + Display_From] = Loaded_Lines[Selected_Line + Display_From].Remove(Loaded_Lines[Selected_Line + Display_From].Length - 1, 1);
+                }
+                else
+                {
+                    if (Selected_Line - 1 >= 0) Selected_Line--;
+                    Loaded_Lines.RemoveAt(Selected_Line + Display_From + 1);
+                }
+                Render_Selected();
+                Update_Display_List();
+                Render_Text();
+                return;
+            }
+
+            if ((int)e.KeyChar == 13)
+            {
+                if (Selected_Line + 1 < 13) Selected_Line++;
+                Loaded_Lines.Insert(Selected_Line + Display_From, "");
+                Render_Selected();
+                Update_Display_List();
+                Render_Text();
+                return;
+            }
+
+            if ((Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                if ((int)e.KeyChar == 19) { Save_File(); return; }
+            }
+
+            if (Selected_Line >= Loaded_Lines.Count) Loaded_Lines.Add("");
+            Loaded_Lines[Selected_Line + Display_From] += e.KeyChar;
+            To_Display[Selected_Line] += e.KeyChar;
+            Render_Text();
+        }
+    }
+}
